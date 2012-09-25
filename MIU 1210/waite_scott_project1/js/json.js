cd929dbcf9e45463d526252dd0268db99491ae55
Scott Waite <scottmwaite@Scotts-MacBook-Pro.local>
2012-09-24 21:23:10 -0400

    Commit 1

---
+++ MIU 1210/waite_scott_project1/js/json.js
@@ -0,0 +1,16 @@
+// Scott Waite
+// MIU 1210
+// Week 1 Project 1
+
+
+var json = {
+	"student1": {
+		"groups": ["Hair Color:", "Blond"],
+		"studentid": ["Student ID:", "23423456"],
+		"firstname": ["First Name:", "Sally"],
+		"lastname": ["Last Name:", "Smith"],
+		"parentemail": ["Parent Email:", "ssmith123@test.com"],
+		"addAssistance": ["Additional Assistance Required:", "10"]
+	}
+
+}
